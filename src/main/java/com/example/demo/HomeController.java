45791e4a6c7f077c9cdc28e17d38d76607be593a
miyad <1505074.msr@ugrad.cse.buet.ac.bd>
2021-06-16 17:01:04 +0600

    pass data in thyemeleaf from java Controller class

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping
-    public String home(){
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("name","Md. Shahibur Rahman");
         return "index"; // return the html page in resources/templates/index.html
         // this works like that(finding index in templates) only because we included thymeleaf in our project
     }
