7fb1558b4b359b3ae0557d117af3facdd2e21cc6
miyad <1505074.msr@ugrad.cse.buet.ac.bd>
2021-06-16 18:51:49 +0600

    variable url path

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +15,7 @@ public class HomeController {
     public String home(Model model){
         model.addAttribute("name","Md. Shahibur Rahman");
         model.addAttribute("val",200);
-        List<String> list = new ArrayList<String>();
+        List<String> list = new ArrayList<>();
         list.add("Miyad");
         list.add("khalid");
         list.add("Dhaka");
@@ -21,5 +23,9 @@ public class HomeController {
         model.addAttribute("list",list);
         return "index"; // return the html page in resources/templates/index.html
         // this works like that(finding index in templates) only because we included thymeleaf in our project
+    }
+    @GetMapping("/{id}")
+    public String page(@PathVariable String id){
+        return id;
     }
 }
